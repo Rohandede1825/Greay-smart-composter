9d4a28b03b29842015aa410427c01d8da0faf2c9
wanilalits <wanilalits@gmail.com>
2025-05-13 14:56:57 +0530

    #

--- src/app/profile/page.jsx
+++ src/app/profile/page.jsx
@@ -25,7 +25,6 @@ import LineGraph from "../Cpmponents/LineGraph";
 
 function page() {
   const [data, setData] = useState({ "Humidity": "...", "Temperature": "...", "Ph": "...", "H2s": "--", "Ammonia": "...", "Methane": "...", "Co2": "..", "time": "updating please wait...", })
-
   const [loading, setLoading] = useState(false);
   const [loading1, setLoading1] = useState(false);
   const [nstartDate, setStartDaten] = useState(new Date());
@@ -38,47 +37,35 @@ function page() {
       const response = await fetch(window.location.origin + '/api/users/sensorslog');
       const result = await response.json();
       setData(await result[0])
-    
+
         .catch(error => {
           return
         });
     }
     catch (error) {
       return
-    
     }
-
   };
 
 
   const getFirstGraphdata = async () => {
-    
-  try {
-    const response = await fetch(window.location.origin + '/api/users/sensorslog?purp=20');
-    const graphData = await response.json();
-   //console.log(graphData)
-    setMy(graphData);
- 
-  } catch (error) {
-    
-  }
- 
+    try {
+      const response = await fetch(window.location.origin + '/api/users/sensorslog?purp=20');
+      const graphData = await response.json();
+      //console.log(graphData)
+      setMy(graphData);
+    } catch (error) {
+    }
   };
-  
-
 
 
 
   useEffect(() => {
-  
     getdata();
     getFirstGraphdata();
-
-    const intervalId = setInterval(getdata, 10000);
+    const intervalId = setInterval(getdata, 30000);
     return () => clearInterval(intervalId);
   }, []);
-
-
   const onLogoff = () => {
     setLoading1(true);
     fetch(window.location.origin + '/api/users/logoff')
@@ -87,21 +74,17 @@ function page() {
       .then(() => {
         router.push('./login')
       })
-
       .catch((error) => {
-           // setLoading1(false);
-    
+        // setLoading1(false);
       });
-
-
   }
 
 
   ////////////////////////////////////////////////////////////////////////////////////////////
   const fetchDataAndCreateExcel = async () => {
     setLoading(true);
- let a =new Date( nstartDate - 5.5 * 60 * 60 * 1000)
- let b=new Date( nendDate - 5.5 * 60 * 60 * 1000)
+    let a = new Date(nstartDate - 5.5 * 60 * 60 * 1000)
+    let b = new Date(nendDate - 5.5 * 60 * 60 * 1000)
 
     try {
       // Example API call
@@ -138,117 +121,72 @@ function page() {
     <>
 
       <div className={profile.bg}>
-       
+
         <div className={profile.Rightpanenl}>
           <div className={profile.UserInfo} >
-           
-           
-          
             <button className={profile.logoff} onClick={(e) => (onLogoff(e))} disabled={loading1}>{loading1 ? 'Logging Off' : ' Log Off '}</button>
-            
-
-
             <h1 style={{ "color": "blue", 'textAlign': 'center' }}>Greya Smart Composter</h1>
             <h4 style={{ 'textAlign': 'center' }}>A Smart IoT-Enabled Device for On-Site Wet Waste Processing and Home Composting</h4>
           </div>
-
-
-
           <div className={profile.Info}>
+            
+            <div>
+              Start Date:&nbsp;   
+              <DateTimePicker
+                amPmAriaLabel="Select AM/PM"
+                calendarAriaLabel="Toggle calendar"
+                clearAriaLabel="Clear value"
+                dayAriaLabel="Day"
+                hourAriaLabel="Hour"
+                maxDetail="second"
+                minuteAriaLabel="Minute"
+                monthAriaLabel="Month"
+                nativeInputAriaLabel="Date and time"
+                onChange={setStartDaten}
+                secondAriaLabel="Second"
+                value={nstartDate}
+                yearAriaLabel="Year"
+                format={"dd-MM-y h:mm:s a"} />
+              &nbsp;&nbsp;
 
-          <div>
-
-            Start Date:&nbsp;   <DateTimePicker
-        amPmAriaLabel="Select AM/PM"
-        calendarAriaLabel="Toggle calendar"
-        clearAriaLabel="Clear value"
-        dayAriaLabel="Day"
-        hourAriaLabel="Hour"
-        maxDetail="second"
-        minuteAriaLabel="Minute"
-        monthAriaLabel="Month"
-        nativeInputAriaLabel="Date and time"
-        onChange={setStartDaten}
-        secondAriaLabel="Second"
-        value={nstartDate}
-        yearAriaLabel="Year"
-        format={"dd-MM-y h:mm:s a"}
-      />&nbsp;&nbsp;
-
-End Date:&nbsp; <DateTimePicker
-        amPmAriaLabel="Select AM/PM"
-        calendarAriaLabel="Toggle calendar"
-        clearAriaLabel="Clear value"
-        dayAriaLabel="Day"
-        hourAriaLabel="Hour"
-        maxDetail="second"
-        minuteAriaLabel="Minute"
-        monthAriaLabel="Month"
-        nativeInputAriaLabel="Date and time"
-        onChange={setEndDaten}
-        secondAriaLabel="Second"
-        value={nendDate}
-        yearAriaLabel="Year"
-        format={"dd-MM-y h:mm:s a"}
-      />
-
-
-&nbsp;&nbsp;
-            <button onClick={fetchDataAndCreateExcel} disabled={loading}>{loading ? 'Generating Excel...' : 'Generate Excel'}</button>
-
-
-</div>
-
-
-
-
+              End Date:&nbsp; 
+              <DateTimePicker
+                amPmAriaLabel="Select AM/PM"
+                calendarAriaLabel="Toggle calendar"
+                clearAriaLabel="Clear value"
+                dayAriaLabel="Day"
+                hourAriaLabel="Hour"
+                maxDetail="second"
+                minuteAriaLabel="Minute"
+                monthAriaLabel="Month"
+                nativeInputAriaLabel="Date and time"
+                onChange={setEndDaten}
+                secondAriaLabel="Second"
+                value={nendDate}
+                yearAriaLabel="Year"
+                format={"dd-MM-y h:mm:s a"} />
+              &nbsp;&nbsp;
+              <button onClick={fetchDataAndCreateExcel} disabled={loading}>{loading ? 'Generating Excel...' : 'Generate Excel'}</button>
+            </div>
 
             <br></br>
-            Last Updates:- {new Date(new Date (data.time)- 5.5 * 60 * 60 * 1000).toLocaleString()}
+            Last Updates:- {new Date(new Date(data.time) - 5.5 * 60 * 60 * 1000).toLocaleString()}
 
-            
+            <div className={profile1.Content}>
 
-
-
-<div className={profile.Content}> 
-              <div className={profile.sensor1}><div className={profile.sensor}><div className={profile.sensorName}>RH</div> <div className={profile.sensorValue}>{data.Humidity}</div>  </div></div>
-              <div className={profile.sensor2}><div className={profile.sensor}><div className={profile.sensorName}>Temp</div><div className={profile.sensorValue}>{data.Temperature}</div></div></div>
-              <div className={profile.sensor3}><div className={profile.sensor}><div className={profile.sensorName}>pH</div><div className={profile.sensorValue}>{data.Ph}</div></div></div>
-              <div className={profile.sensor4}><div className={profile.sensor}><div className={profile.sensorName}>H2S</div><div className={profile.sensorValue}>{data.H2s}</div></div></div>
-              <div className={profile.sensor5}><div className={profile.sensor}><div className={profile.sensorName}>NH3</div><div className={profile.sensorValue}>{data.Ammonia} </div></div></div>
-              <div className={profile.sensor6}><div className={profile.sensor}><div className={profile.sensorName}>CH4</div><div className={profile.sensorValue}>{data.Methane}</div></div></div>
-              <div className={profile.sensor7}><div className={profile.sensor}><div className={profile.sensorName}>Co2    </div><div className={profile.sensorValue}>{data.Co2}</div></div></div>
-            </div>
-
-
-
-
-
-
-<div className={profile1.Content}>
-
-<div className={profile1.container} style={{backgroundColor: 'rgb(182, 130, 99)'}}><div className={profile1.heading} ><Image src={humidity} className={profile.img} width={40} height={40} alt=""/> <div className={profile1.sensorName}>Humidity</div> <div className={profile1.sensorValue}>{data.Humidity}</div> <div className={profile1.sensorUnit}>%</div> </div> <LineGraph data={data.Humidity} time={data.time} Label={'Humidity'} priviousData={my} mykey={'Humidity'} /></div>
-<div className={profile1.container} style={{backgroundColor: 'rgb(161, 155, 92)'}}><div className={profile1.heading} ><Image src={temperature} className={profile.img} width={40} height={40} alt=""/><div className={profile1.sensorName}>Temperature</div><div className={profile1.sensorValue}>{data.Temperature}</div><div className={profile1.sensorUnit}>&#176;C</div></div><LineGraph data={data.Temperature} time={data.time} Label={'Temperature'}  priviousData={my} mykey={'Temperature'}/></div>    
-<div className={profile1.container} style={{backgroundColor: 'rgb(123, 168, 105)'}}><div className={profile1.heading} ><Image src={ph} className={profile.img} width={40} height={40} alt="" /><div className={profile1.sensorName}>pH</div><div className={profile1.sensorValue}>{data.Ph}</div><div className={profile1.sensorUnit}>pH</div></div><LineGraph data={data.Ph} time={data.time} Label={'pH'}  priviousData={my} mykey={'Ph'} /></div> 
-<div className={profile1.container} style={{backgroundColor: 'rgb(166, 185, 211)'}}><div className={profile1.heading} ><Image src={H2S} className={profile.img} width={40} height={40} alt="" /><div className={profile1.sensorName}>H2S</div><div className={profile1.sensorValue}>{data.H2s}</div><div className={profile1.sensorUnit}>ppm</div></div><LineGraph data={data.H2s} time={data.time} Label={'H2S'}  priviousData={my} mykey={'H2s'}/></div>
-<div className={profile1.container} style={{backgroundColor: 'rgb(197, 146, 194)'}}><div className={profile1.heading}><Image src={NH3} className={profile.img} width={40} height={40} alt="" /><div className={profile1.sensorName}>Ammonia</div><div className={profile1.sensorValue}>{data.Ammonia} </div><div className={profile1.sensorUnit}>ppm</div></div><LineGraph data={data.Ammonia} time={data.time} Label={'Ammonia'}  priviousData={my} mykey={'Ammonia'} /></div>
-<div className={profile1.container} style={{backgroundColor: 'rgb(236, 137, 174)'}}><div className={profile1.heading} ><Image src={CH4} className={profile.img} width={40} height={40} alt="" /><div className={profile1.sensorName}>Methane</div><div className={profile1.sensorValue}>{data.Methane}</div><div className={profile1.sensorUnit}>ppm</div></div><LineGraph data={data.Methane} time={data.time} Label={'Methane'}  priviousData={my} mykey={'Methane'} /></div>    
-<div className={profile1.container} style={{backgroundColor: 'rgb(175, 130, 132)'}}><div className={profile1.heading} ><Image src={CO2} className={profile.img} width={40} height={40} alt="" /><div className={profile1.sensorName}>Co2    </div><div className={profile1.sensorValue}>{data.Co2}</div><div className={profile1.sensorUnit}>ppm</div></div><LineGraph data={data.Co2} time={data.time} Label={'CO2'}  priviousData={my} mykey={'Co2'}/></div>
+              <div className={profile1.container} style={{ backgroundColor: 'rgb(182, 130, 99)' }}><div className={profile1.heading} ><Image src={humidity} className={profile.img} width={40} height={40} alt="" /> <div className={profile1.sensorName}>Humidity</div> <div className={profile1.sensorValue}>{data.Humidity}</div> <div className={profile1.sensorUnit}>%</div> </div> <LineGraph data={data.Humidity} time={data.time} Label={'Humidity'} priviousData={my} mykey={'Humidity'} /></div>
+              <div className={profile1.container} style={{ backgroundColor: 'rgb(161, 155, 92)' }}><div className={profile1.heading} ><Image src={temperature} className={profile.img} width={40} height={40} alt="" /><div className={profile1.sensorName}>Temperature</div><div className={profile1.sensorValue}>{data.Temperature}</div><div className={profile1.sensorUnit}>&#176;C</div></div><LineGraph data={data.Temperature} time={data.time} Label={'Temperature'} priviousData={my} mykey={'Temperature'} /></div>
+              <div className={profile1.container} style={{ backgroundColor: 'rgb(123, 168, 105)' }}><div className={profile1.heading} ><Image src={ph} className={profile.img} width={40} height={40} alt="" /><div className={profile1.sensorName}>pH</div><div className={profile1.sensorValue}>{data.Ph}</div><div className={profile1.sensorUnit}>pH</div></div><LineGraph data={data.Ph} time={data.time} Label={'pH'} priviousData={my} mykey={'Ph'} /></div>
+              <div className={profile1.container} style={{ backgroundColor: 'rgb(166, 185, 211)' }}><div className={profile1.heading} ><Image src={H2S} className={profile.img} width={40} height={40} alt="" /><div className={profile1.sensorName}>H2S</div><div className={profile1.sensorValue}>{data.H2s}</div><div className={profile1.sensorUnit}>ppm</div></div><LineGraph data={data.H2s} time={data.time} Label={'H2S'} priviousData={my} mykey={'H2s'} /></div>
+              <div className={profile1.container} style={{ backgroundColor: 'rgb(197, 146, 194)' }}><div className={profile1.heading}><Image src={NH3} className={profile.img} width={40} height={40} alt="" /><div className={profile1.sensorName}>Ammonia</div><div className={profile1.sensorValue}>{data.Ammonia} </div><div className={profile1.sensorUnit}>ppm</div></div><LineGraph data={data.Ammonia} time={data.time} Label={'Ammonia'} priviousData={my} mykey={'Ammonia'} /></div>
+              <div className={profile1.container} style={{ backgroundColor: 'rgb(236, 137, 174)' }}><div className={profile1.heading} ><Image src={CH4} className={profile.img} width={40} height={40} alt="" /><div className={profile1.sensorName}>Methane</div><div className={profile1.sensorValue}>{data.Methane}</div><div className={profile1.sensorUnit}>ppm</div></div><LineGraph data={data.Methane} time={data.time} Label={'Methane'} priviousData={my} mykey={'Methane'} /></div>
+              <div className={profile1.container} style={{ backgroundColor: 'rgb(175, 130, 132)' }}><div className={profile1.heading} ><Image src={CO2} className={profile.img} width={40} height={40} alt="" /><div className={profile1.sensorName}>Co2    </div><div className={profile1.sensorValue}>{data.Co2}</div><div className={profile1.sensorUnit}>ppm</div></div><LineGraph data={data.Co2} time={data.time} Label={'CO2'} priviousData={my} mykey={'Co2'} /></div>
 
             </div>
-
-
           </div>
-
-
         </div>
-
-
-
-
-
-
       </div>
+
 
 
 
@@ -257,7 +195,4 @@ End Date:&nbsp; <DateTimePicker
   );
 }
 export default page;
-//https://api.thingspeak.com/channels/2901124/feeds.json?results=1
-//<Image src={facebook} width={30} height={30}  alt="GFG logo imported from public directory"  />
 
-// <div className={profile.sensor2}> <div className={profile.sensor}>><div className={profile.sensorName}>Temperature</div><div className={profile.sensorValue}>{data.Temperature}</div><div className={profile.sensorUnit}>C</div></div></div>
